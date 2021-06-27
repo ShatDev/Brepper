1fb6085654b302c21c18ff2042eae8a64342a2fd
agxmbhir <agambhir5656@gmail.com>
2021-06-27 16:22:41 +0530

    changes

--- public/index.js
+++ public/index.js
@@ -24,24 +24,53 @@ window.addEventListener('load', () => {
       console.log(instance.methods.createFunding);
     
     });
-    
   });
 });
+var accountInterval = setInterval(function() {
+  if (web3.eth.accounts[0] !== account) {
+    account = web3.eth.accounts[0];
+    updateInterface();
+  }
+}, 100);
 
 const form = document.querySelector("#create-form");
 const createButton = document.getElementById("create-button");
 const goal = document.getElementById("funding-goal")
 
-const getAccounts = async function() { 
-const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-const account = accounts[0]
+
+var noOfContracts = 0;
+// make sure this function returns all accounts
+const getAccounts = async () => {
+  return await window.ethereum.request({ method: 'eth_requestAccounts' })
 }
-await getAccounts()
 
-form.addEventListener("submit", function(e) {
-  e.preventDefault();
-  instance.methods.createFunding(goal.value , 40000).send({from: account })
+// use the above function in your event listener:
+form.addEventListener('submit', async e => {
+  e.preventDefault()
+  // get up to date account data when you already submit the form
+  const [account] = await getAccounts() // just destructuring result to get the first account
+  instance.methods.createFunding(goal.value, 40000).send({ from: account }).then((value) => {console.log(value)})
+
+  noOfContracts++;
 })
+ 
+const getFundAddress = async (i) => { return await instance.methods.fundings(noOfContracts).call() }
 
+// document.querySelector(".body-div").addEventListener("load", () =>{
+// getWeb3().then(async (web3) => {
 
+//   $.getJSON("/CrownFunding.json", (CrowdFunding) => {
+//     const fund = new web3.eth.Contract(
+//       CrowdFunding.abi,
+//     await getFundAddress(noOfContracts)
+//     );
+//     window.fund = fund;
+//     console.log(fund);
+  
+//   });
+// }); 
+// })
 
+window.ethereum.on('accountsChanged', function (accounts) {
+  // Time to reload your interface with accounts[0]!
+})

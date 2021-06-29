4e4f1a27280e34182e707ab0966d0d384f995f7a
jpuri <jyotipuri@gmail.com>
2021-06-29 18:40:11 +0530

    More changes on fundraiser page

--- contracts/Brepper.sol
+++ contracts/Brepper.sol
@@ -18,6 +18,11 @@ contract FundingCreator {
         CrowdFunding funding = fundings[_fundingindex];
         return funding.goal();
     }
+
+     function getFundingContractAddress(uint _fundingindex) public view returns (address) {
+        CrowdFunding funding = fundings[_fundingindex];
+        return address(funding);
+    }
 }
 
 contract CrowdFunding {
--- public/fund.js
+++ public/fund.js
@@ -17,18 +17,29 @@ window.addEventListener('load', () => {
   getWeb3().then(async (web3) => {
     const networkId = await web3.eth.net.getId();
     $.getJSON("/FundingCreator.json", (FundingCreatorContract) => {
-      const deployedNetwork = FundingCreatorContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        FundingCreatorContract.abi,
-        deployedNetwork && deployedNetwork.address
-      );
-      const urlSearchParams = new URLSearchParams(window.location.search);
-      const params = Object.fromEntries(urlSearchParams.entries());
+      $.getJSON("/CrowdFunding.json", (CrowdFundingContract) => {
+        const deployedNetwork = FundingCreatorContract.networks[networkId];
+        const instance = new web3.eth.Contract(
+          FundingCreatorContract.abi,
+          deployedNetwork && deployedNetwork.address
+        );
+        const urlSearchParams = new URLSearchParams(window.location.search);
+        const params = Object.fromEntries(urlSearchParams.entries());
         (async () => {
-          const val = await instance.methods.getFundingContract(params.index).call();
-          // here we need to get and show more details
-          console.log(val)
+          const address = await instance.methods.getFundingContractAddress(params.index).call();
+          const fundraiserInstance = new web3.eth.Contract(
+            CrowdFundingContract.abi,
+            address
+          );
+          console.log(fundraiserInstance)
+          const goal = await fundraiserInstance.methods.goal().call()
+          document.getElementById('goal_value').innerText = goal
+          const deadline = await fundraiserInstance.methods.deadline().call()
+          document.getElementById('deadline_value').innerText = deadline
+          const raisedAmount = await fundraiserInstance.methods.raisedAmount().call()
+          document.getElementById('raisedAmount_value').innerText = raisedAmount
         })()
+      });
     });
   });
 });

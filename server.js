6bf6031ac870138e55d568249aeb05b63e07ed5d
agxmbhir <agambhir5656@gmail.com>
2021-07-02 20:55:06 +0530

    removed yarn

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ app.get("/CrowdFunding.json" , function(req,res) {
 app.get("/FundingCreator.json" , function(req,res) {
   res.sendFile(__dirname + "/build/contracts/FundingCreator.json")
 })
-
+app.get("/img/mango.png", function(req, res) {
+  res.sendFile()
+}
 app.listen(3000, function() {
   console.log("Listening on port 3000");
 });
